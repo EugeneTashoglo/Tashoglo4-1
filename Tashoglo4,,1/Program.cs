57ffbfa8244ec566b57bdc06613f55b1f03f7bf9
Евгений Ташогло <tashoglo00@mail.ru>
2021-11-29 16:00:04 +0200

    Добавьте файлы проекта.

---
+++ Tashoglo4,,1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _24variant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                int[] mas = new int[12];
+                Random rnd = new Random();
+                for (int i = 0; i < mas.Length; i++)
+                {
+                    mas[i] = rnd.Next(10, 99);
+                    Console.Write(mas[i] + " ");
+                }
+                Console.WriteLine("Введите последний элемент массива: ");
+                int n = int.Parse(Console.ReadLine());
+                if (n == (mas[mas.Length - 1]))
+                {
+                    int sum = 0;
+                    for (int i = 0; i < n; i++)
+                    {
+                        sum += mas[i];
+                    }
+                    Console.WriteLine($"Сумма={sum}");
+
+                }
+                else Console.WriteLine("Это не последний элемент массива");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
